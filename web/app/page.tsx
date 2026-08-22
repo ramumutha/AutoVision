98f2525f362b84d192e39215ecda7005d3af9efa
ramumutha <ramumutha1980@gmail.com>
2026-08-22 12:02:22 +0530

    chore: establish Service Profit AI R1 frontend baseline

--- web/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { VehicleSearch } from "@/components/vehicle-search";
-
-export default function HomePage() {
-  return <VehicleSearch />;
-}
--- web/app/vehicles/[vehicleId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { VehicleDetail } from "@/components/vehicle-detail";
-
-export default function VehicleDetailPage() {
-  return <VehicleDetail />;
-}
